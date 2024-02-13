55cc3b52721e017b8a10c21ce4fcf2ae8492da19
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-02-13 20:02:32 +0100

    Add baapp.py functions and basic layout

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+import tmdb_client
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = []
-    return render_template("homepage.html", movies=list(range(0, 4)))
+    movies = tmdb_client.get_movies(how_many=8)
+    return render_template("homepage.html", movies=movies)
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+@app.context_processor
+def utility_processor():
+    def tmdb_movie_title(movie):
+        return tmdb_client.get_movie_title(movie)
+    return {"tmdb_movie_title": tmdb_movie_title}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tmdb_client.py
@@ -0,0 +1,25 @@
+import requests
+from PERSONAL_API_KEY import PERSONAL_API_KEY
+
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f'Bearer {PERSONAL_API_KEY}'
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_movies(how_many):
+    data = get_popular_movies()
+    return data["results"][:how_many]
+
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+
+def get_movie_title(movie):
+    return str(movie['title'])
