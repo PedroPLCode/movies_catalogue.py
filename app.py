df9c0edb68960e77a9859e0aa03c97837ac48f49
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-02-13 19:07:28 +0100

    Add basic project structure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    movies = []
     return render_template("homepage.html", movies=list(range(0, 4)))
 
 if __name__ == '__main__':
