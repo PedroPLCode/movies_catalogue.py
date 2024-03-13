a9772abdaf67ac1f708e5e2516a584547a32dd91
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-03-13 09:23:23 +0100

    Add pytest test_flask.py

---
+++ tests/test_flask.py
@@ -0,0 +1,21 @@
+from app import app
+from unittest.mock import Mock
+import pytest
+
+@pytest.mark.parametrize('path, arg', (
+    ('/', 'movie/popular'),
+    ('/?list_type=WRONG_LIST_TYPE', 'movie/popular'),
+    ('/?list_type=top_rated', 'movie/top_rated'),
+    ('/?list_type=upcoming', 'movie/upcoming'),
+    ('/?list_type=now_playing', 'movie/now_playing'),
+    ('/search?q=TEST_QUERY', 'search/movie?query=TEST_QUERY'),
+    ('/today?timezone=TEST_QUERY', 'tv/airing_today?timezone=TEST_QUERY'),
+))
+def test_homepage(path, arg, monkeypatch):
+    api_mock = Mock(return_value={'results': []})
+    monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+
+    with app.test_client() as client:
+        response = client.get(path)
+        assert response.status_code == 200
+        api_mock.assert_called_once_with(arg)
